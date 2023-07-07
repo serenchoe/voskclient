1eae5c599cf99509e5b758884fd2fc0ea149da5a
serenchoe <serenchoe@users.noreply.github.com>
2023-07-07 17:04:51 +0900

    try with new container image

--- voice_client_with_script_processor.js
+++ voice_client_with_script_processor.js
@@ -6,7 +6,7 @@ var webSocket;
 var inputArea;
 const bufferSize = 8192;
 const sampleRate = 8000;
-const wsURL = 'ws://a2b988289a9494c35902ae52a17259de-1617582533.ap-northeast-2.elb.amazonaws.com:32216';
+const wsURL = 'wss://acc2bc06a6b3a482480a84868a592405-1001187077.ap-northeast-2.elb.amazonaws.com:31836';
 var initComplete = false;
 
 (function () {
